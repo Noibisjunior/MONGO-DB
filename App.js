714ac6cce901b89dfb34e1c57f933ff6076f5b4d
Noibisjunior <noibisjunior22@gmail.com>
2022-11-17 11:12:36 +0100

    file

--- App.js
+++ App.js
@@ -4,36 +4,37 @@
 3.create a compare password using bcrypt.compare done but install bcrypt
 4.create express-handle-bars to create a dynamic web application done but install express
 5.display a visual error message using render method done
-6.create a nodemailer to send an email to successful registerd users*/
+6.create a nodemailer to send an email to successful registerd users*/ 
 
-require('dotenv').config();
+require('dotenv').config()
 
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 5000
 const connectDB = require('./DATABASE/connect');
 const router = require('./routes/auth');
-const { engine } = require('express-handlebars');
+const {engine} = require('express-handlebars');
 const path = require('path');
 // app.use(express.static('public'))
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
-app.use(express.json());
+app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', router);
+app.use('/', router)
 // app.use('landingpage')
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, () => console.log(`Server is listening on port ${port}`));
-  } catch (error) {
+    app.listen(port, () => console.log(`Server is listening on port ${port}`
+    ))
+  }
+   catch (error) {
     console.log(error);
   }
 };
-
 
 
 
